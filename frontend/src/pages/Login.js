a75ab85433fe3fb2b2374f3e69a9c8701da317f4
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-16 14:02:11 +0300

    react-icons installed on the project

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,29 +1,30 @@
 import React from "react";
-import '../style/Login.css';
-import google from '../assets/icons/google.svg';
-import github from '../assets/icons/github.svg';
+import "../style/Login.css";
+import {FcGoogle} from "react-icons/fc";
 
 const Login = () => {
-    return(
-        <div className="container-fluid-custom bg-secondary">
-            <div className="row menu-row mx-auto">
-                <div className="col-md-6 col-6 text-center">
-                    <button className="menu-button active">SIGN UP</button>
-                </div>
-                <div className="col-md-6 col-6 text-center">
-                    <button className="menu-button">SIGN IN</button>
-                </div>
-            </div>
-            <div className="row mx-auto">
-                <div className="col-md-6 col-12 mx-auto">
-                    <div className="row mx-auto">
-                        <div className="col-md-3 col-3 mx-auto">
-                        <img src={google} alt="google" className="icon"/>
-                            </div>
-                            </div>
-                            </div></div>
+  return (
+    <div className="container-fluid-custom bg-secondary">
+      <div className="row menu-row mx-auto">
+        <div className="col-md-6 col-6 text-center">
+          <button className="menu-button active">SIGN UP</button>
         </div>
-    );
-}
+        <div className="col-md-6 col-6 text-center">
+          <button className="menu-button">SIGN IN</button>
+        </div>
+      </div>
+      <div className="row mx-auto">
+        <div className="col-md-6 col-12 mx-auto">
+          <div className="row mx-auto">
+            <div className="col-md-3 col-3 border social-button">
+            <FcGoogle className="google-icon"/>
+            h
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
