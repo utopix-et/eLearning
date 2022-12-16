fe3ac73bc8d293f9acb6e7e81cc0f0e927e8c62c
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-16 10:34:57 +0300

    Login sample file renderred inside index.js file

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Login = () => {
+    return(
+        <div className="container-fluid bg-secondary">
+            <div className="row">
+                <div className="col-6">
+                    <h1>Left Side</h1>
+                </div>
+                <div className="col-6">
+                    <h1>Right Side</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
