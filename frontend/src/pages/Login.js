2ef84de51b99fc7b84049e6cabc3bba2f5137516
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-16 11:47:31 +0300

    Login page menu bar added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,11 +4,11 @@ import '../style/Login.css';
 const Login = () => {
     return(
         <div className="container-fluid bg-secondary">
-            <div className="row menu-row bg-warning mx-auto">
-                <div className="col-6 text-center">
-                    <button className="menu-button">Left Side</button>
+            <div className="row menu-row mx-auto">
+                <div className="col-md-6 col-6 text-center">
+                    <button className="menu-button active">Left Side</button>
                 </div>
-                <div className="col-6 text-center">
+                <div className="col-md-6 col-6 text-center">
                     <button className="menu-button">Right Side</button>
                 </div>
             </div>
