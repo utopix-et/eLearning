33af694cb14a72dfede055bffcde878deb17669c
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-16 11:37:12 +0300

    Login row styles added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,12 +4,12 @@ import '../style/Login.css';
 const Login = () => {
     return(
         <div className="container-fluid bg-secondary">
-            <div className="row w-50 bg-warning mx-auto">
+            <div className="row menu-row bg-warning mx-auto">
                 <div className="col-6 text-center">
-                    <button>Left Side</button>
+                    <button className="menu-button">Left Side</button>
                 </div>
-                <div className="col-6  text-center">
-                    <button>Right Side</button>
+                <div className="col-6 text-center">
+                    <button className="menu-button">Right Side</button>
                 </div>
             </div>
         </div>
