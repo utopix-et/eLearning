f07a35c75c5c9cc5e91ce1c6b94f342ddf494076
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-16 11:23:56 +0300

    login styling file created

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,14 @@
 import React from "react";
+import '../style/Login.css';
 
 const Login = () => {
     return(
         <div className="container-fluid bg-secondary">
             <div className="row w-50 bg-warning mx-auto">
-                <div className="col-6 bg-primary mx-auto">
+                <div className="col-6 text-center">
                     <button>Left Side</button>
                 </div>
-                <div className="col-6">
+                <div className="col-6  text-center">
                     <button>Right Side</button>
                 </div>
             </div>
