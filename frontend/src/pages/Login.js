c01f5d6a57f00667249f3d96435ae94c6cddfe0f
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-16 11:53:30 +0300

    the two column text changed to signup and signin

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -6,10 +6,10 @@ const Login = () => {
         <div className="container-fluid-custom bg-secondary">
             <div className="row menu-row mx-auto">
                 <div className="col-md-6 col-6 text-center">
-                    <button className="menu-button active">Left Side</button>
+                    <button className="menu-button active">SIGN UP</button>
                 </div>
                 <div className="col-md-6 col-6 text-center">
-                    <button className="menu-button">Right Side</button>
+                    <button className="menu-button">SIGN IN</button>
                 </div>
             </div>
         </div>
