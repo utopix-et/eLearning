6443235ab83e1d3261bd70e89683785f01d6f9f9
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-16 12:29:42 +0300

    fontawesome icons added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../style/Login.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 const Login = () => {
     return(
@@ -12,6 +13,14 @@ const Login = () => {
                     <button className="menu-button">SIGN IN</button>
                 </div>
             </div>
+            <div className="row mx-auto">
+                <div className="col-md-6 col-12 mx-auto">
+                    <div className="row mx-auto">
+                        <div className="col-md-3 col-3 mx-auto">
+                        <i class="fa-brands fa-google"></i>
+                            </div>
+                            </div>
+                            </div></div>
         </div>
     );
 }
