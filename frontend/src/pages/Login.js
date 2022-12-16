b060c14df3e343f7305f06019727e40b9405ebec
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-16 20:43:54 +0300

    Login forms added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,27 +14,53 @@ const Login = () => {
           <button className="menu-button">SIGN IN</button>
         </div>
       </div>
-      
-    <div className="container-fluid-custom mx-auto">
-      <div className="social-row mx-auto bg-white">
-        <div className="col-md-12 col-12 mx-auto">
-          <div className="row mx-auto">
-            <div className="col-md-5 col-10 rounded social-button btn">
-              <FcGoogle className="google-icon" size={30} />
-              <span className="social-text ps-2">Continue with Google</span>
-            </div>
-            <div className="col-md-5 col-10 rounded social-button btn">
-              <BsGithub className="github-icon" size={30} />
-              <span className="social-text ps-2">Continue with Github</span>
+
+      <div className="container-fluid-custom mx-auto">
+        <div className="social-row mx-auto bg-white">
+          <div className="col-md-12 col-12 mx-auto">
+            <div className="row mx-auto">
+              <div className="col-md-5 col-11 rounded social-button btn pt-2">
+                <FcGoogle className="google-icon" size={30} />
+                <span className="social-text ps-2">Continue with Google</span>
+              </div>
+              <div className="col-md-5 col-11 rounded social-button btn pt-2">
+                <BsGithub className="github-icon " size={30} />
+                <span className="social-text ps-2">Continue with Github</span>
+              </div>
             </div>
           </div>
         </div>
+        <div className="mx-auto text-center pb-3 bg-white">Or</div>
+        
+        <div className="form-row mx-auto bg-white mt-4">
+          <div className="col-md-12 col-12 mx-auto">
+            <div className="row mx-auto">
+              <div className="col-md-10 col-12 mx-auto">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="col-md-10 col-12 mx-auto my-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="col-md-10 col-12 mx-auto my-4">
+            <button className="btn btn-primary form-control">SIGN IN</button>
+            </div>
+            <div className="col-md-10 col-12 mx-auto text-center my-2">
+            Forgot Password?
+            <a className="text-decoration-none" href="#Forgot"> Reset</a>
+            </div>
+            </div>
+           
+          </div>
+        </div>
       </div>
-      <div className="mx-auto text-center mt-3 bg-white">
-      Or
-      </div>
-      
-    </div>
     </div>
   );
 };
