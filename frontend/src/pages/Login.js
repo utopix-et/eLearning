a204cee0e909decc95208912a789faf6bf2868a6
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-16 10:36:21 +0300

    centeral row width chnaged to 50%

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
     return(
         <div className="container-fluid bg-secondary">
-            <div className="row">
+            <div className="row w-50 bg-warning mx-auto">
                 <div className="col-6">
                     <h1>Left Side</h1>
                 </div>
