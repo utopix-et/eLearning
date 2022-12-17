06c0465a944324bf7b22ce5884e272ea6ad4fd77
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-17 20:55:09 +0300

    Signup Page final

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import 'bootstrap/dist/js/bootstrap.min.js';
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 
 function App() {
     return(
         <div className="container-fluid bg-secondary">
-            <Login/>
+            <Signup/>
       </div>
     );
 }
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -4,7 +4,7 @@ import "../style/Login.css";
 import { FcGoogle } from "react-icons/fc";
 import { BsGithub } from "react-icons/bs";
 
-const Login = () => {
+const Signup = () => {
   return (
     <div className="container-fluid mx-auto py-3">
         <div className="row my-4">
@@ -27,16 +27,16 @@ const Login = () => {
             <div className="row mx-auto">
               <div className="col-md-5 col-11 rounded social-button btn pt-2">
                 <FcGoogle className="google-icon" size={30} />
-                <span className="social-text ps-2">Continue with Google</span>
+                <span className="social-text ps-2">Signup with Google</span>
               </div>
               <div className="col-md-5 col-11 rounded social-button btn pt-2">
                 <BsGithub className="github-icon " size={30} />
-                <span className="social-text ps-2">Continue with Github</span>
+                <span className="social-text ps-2">Signup with Github</span>
               </div>
             </div>
           </div>
         </div>
-        <div className="mx-auto text-center pb-3 bg-white">Or</div>
+        <div className="mx-auto text-center py-3 bg-white">Or</div>
         
         <div className="form-row mx-auto bg-white mt-4">
           <div className="col-md-12 col-12 mx-auto">
@@ -56,19 +56,19 @@ const Login = () => {
                 />
               </div>
 
-              <div className="col-md-10 col-12 mx-auto my-4">
+              <div className="col-md-10 col-12 mx-auto mb-4">
                 <input
                   type="text"
                   className="form-control"
                   placeholder="Password"
                 />
               </div>
+            <div className="col-md-10 col-12 mx-auto mb-2">
+            <input type="file" className="form-control" />
+            </div>
+
               <div className="col-md-10 col-12 mx-auto my-4">
             <button className="btn btn-primary form-control">SIGN IN</button>
-            </div>
-            <div className="col-md-10 col-12 mx-auto text-center my-2">
-            Forgot Password?
-            <a className="text-decoration-none" href="#Forgot"> Reset</a>
             </div>
             </div>
            
@@ -79,4 +79,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
