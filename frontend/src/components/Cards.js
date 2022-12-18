cdcb3cab881f1495b113fd019ba9f14b094c3b93
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-18 11:55:58 +0300

    My Course title creatd for Dashboard file

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -1,44 +1,35 @@
 import React from "react";
-import {GoPrimitiveDot} from "react-icons/go";
-import {WiMoonWaningCrescent1} from "react-icons/wi";
+import { GoPrimitiveDot } from "react-icons/go";
+import { WiMoonWaningCrescent1 } from "react-icons/wi";
 
-const Cards = ({ img, levelIcon }) => {
+const Cards = ({ img, levelIcon, Title }) => {
   if (!img) {
     return <h1>Image not found</h1>;
   }
 
-function Icon (){
-    if (levelIcon === "beginner") {
-        return(
-            <GoPrimitiveDot className="text-success"/>
-          )
-    }
-    else{
-        return(
-            <WiMoonWaningCrescent1 className="text-success"/>
-          )
+  function Icon() {
+    if (levelIcon === "Beginner") {
+      return <GoPrimitiveDot className="text-success" />;
+    } else {
+      return <WiMoonWaningCrescent1 className="text-warning me-2" style = {{transform: 'rotate(90deg)' }} />;
     }
   }
 
   return (
     <>
-          <div className="card">
-            <img
-              src={img}
-              className="card-img-top"
-              alt="frontend-foundation-img"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Frontend Foundation HTML + CSS</h5>
-              <div className="py-2">
-                <Icon/>
-                Beginner
-              </div>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
+      <div className="card">
+        <img src={img} className="card-img-top" alt="frontend-foundation-img" />
+        <div className="card-body">
+          <h5 className="card-title">{Title}</h5>
+          <div className="py-2">
+            <Icon />
+            <span>{levelIcon}</span>
+          </div>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
       </div>
     </>
   );
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -13,15 +13,24 @@ const Dashboard = () => {
       <div className="row text-center pb-5 pt-4">
         <h3>Dashboard</h3>
       </div>
+
+      <div className="row Main-row p-4">
+        <div className="row py-2">
+        <h3>My Courses</h3>
+        
+        </div>
       
-      <div className="card-row p-4">
+      <div className="row pb-5 pt-4">
         <div className="col-md-3 col-12">
-        <Cards img={img1} levelIcon="beginner"/>
+          <Cards img={img1} levelIcon="Beginner" Title="Frontend Foundation HTML + CSS"/>
         </div>
         <div className="col-md-3 col-12 ms-5">
-        <Cards img={img2} levelIcon="beginner"/>
+          <Cards img={img2} levelIcon="Intermediate" Title="Javascript for Developer"/>
         </div>
-        </div>
+      </div>
+      
+      </div>
+      
     </>
   );
 };
