3c6c4fdc7b3b24e372077d9236ca03afe4f96a58
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-18 11:32:26 +0300

    cards minor bug fixed

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React from "react";
+import {GoPrimitiveDot} from "react-icons/go";
 
-const Cards = ({img, levelIcon}) =>{
-    return(
-        <>
-        <div className="card-row p-4">
+const Cards = ({ img, levelIcon }) => {
+  if (!img) {
+    return <h1>Image not found</h1>;
+  }
+
+function Icon (){
+    if (levelIcon === "beginner") {
+        return(
+            <GoPrimitiveDot className="text-success"/>
+          )
+    }
+    else{
+        return(
+            <GoPrimitiveDot className="text-success"/>
+          )
+    }
+  }
+
+  return (
+    <>
+      <div className="card-row p-4">
         <div className="col-md-3 col-12">
           <div className="card">
-                <img src={img} className="card-img-top" alt="frontend-foundation-img" />
+            <img
+              src={img}
+              className="card-img-top"
+              alt="frontend-foundation-img"
+            />
             <div className="card-body">
-              <h5 className="card-title">
-                Frontend Foundation HTML + CSS
-              </h5>
+              <h5 className="card-title">Frontend Foundation HTML + CSS</h5>
               <div className="py-2">
-                <levelIcon className="text-success" />
+                <Icon/>
                 Beginner
-                </div>
+              </div>
               <p className="card-text">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
@@ -22,8 +42,9 @@ const Cards = ({img, levelIcon}) =>{
             </div>
           </div>
         </div>
-      </div></>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Cards;
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -3,8 +3,6 @@ import React from "react";
 import Cards from "../components/Cards";
 
 import img1 from "../assets/Images/CourseImages/1.jpg";
-import {GoPrimitiveDot} from "react-icons/go";
-
 
 import "../style/Dashboard.css";
 
@@ -14,7 +12,7 @@ const Dashboard = () => {
       <div className="row text-center pb-5 pt-4">
         <h3>Dashboard</h3>
       </div>
-        <
+        <Cards img={img1} levelIcon="beginner"/>
     </>
   );
 };
