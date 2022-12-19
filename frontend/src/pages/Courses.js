e05e49e050cdfa07711e09f29e0946a01cc0f7de
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-19 19:01:12 +0300

    Custom-card style property created

--- frontend/src/pages/Courses.js
+++ frontend/src/pages/Courses.js
@@ -21,24 +21,24 @@ const Courses = () => {
         </div>
 
         <div className="row pb-5 pt-4">
-          <div className="col-md-3 col-12 border-none">
+          <div className="col-md-3 col-12 custom-card">
             <Cards
               img={img1}
               levelIcon="Beginner"
               Title="Frontend Foundation HTML + CSS"
             />
           </div>
-          <div className="col-md-3 col-12">
+          <div className="col-md-3 col-12 custom-card">
             <Cards
               img={img2}
               levelIcon="Intermediate"
               Title="Javascript for Developer"
             />
           </div>
-          <div className="col-md-3 col-12 ">
+          <div className="col-md-3 col-12 custom-card">
             <Cards img={img3} levelIcon="Beginner" Title="Backend Basics" />
           </div>
-          <div className="col-md-3 col-12">
+          <div className="col-md-3 col-12 custom-card">
             <Cards img={img3} levelIcon="Beginner" Title="Backend Basics" />
           </div>
         </div>
