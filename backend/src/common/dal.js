c68b31570e73af5e3107755f15546c3b731c83ad
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-12-20 10:28:50 +0300

    route update and DAL edit

--- backend/src/common/dal.js
+++ backend/src/common/dal.js
@@ -1,6 +1,11 @@
+const { model } = require("mongoose");
+
 const getOne = (model) => {
   return async (props) => {
-    return await model.findOne(props);
+    const response = await model.findOne(props, { password: 0 });
+    // console.log('res')
+    // console.log(response)
+    return response;
   };
 };
 
@@ -12,10 +17,29 @@ const createOne = (model) => async (props) => {
   return await model.create(props);
 };
 
+const updateOne =
+  (model) =>
+  async (filter = {}, props) => {
+    return await model.findOneAndUpdate(
+      filter,
+      {
+        $set: props.set || new Object(),
+        $push: props.push || new Object(),
+        $pop: props.pop || new Object(),
+        $inc: props.inc || new Object(),
+      },
+      { upsert: true }
+    );
+  };
+const deleteOne = (model) => async (filter) => {
+  return await model.findAndDelete(filter);
+};
 const dataAccessLayer = (model) => ({
   getOne: getOne(model),
   getMany: getAll(model),
   createOne: createOne(model),
+  updateOne: updateOne(model),
+  deleteOne: deleteOne(model),
 });
 
 module.exports = dataAccessLayer;
