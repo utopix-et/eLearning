c308e9157ab80c50e0e8d2be3c960bc1f1c06bd3
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-12-21 10:27:16 +0300

    Added completion check and completion updating methods to the controller and routes files

--- backend/src/resources/completions/routes.js
+++ backend/src/resources/completions/routes.js
@@ -1,5 +1,18 @@
-const router = require("express").Router();
+const router = require("express").Router(),
+  {
+    lessonCompletion,
+    courseCompletion,
+    sectionCompletion,
+    completeCourse,
+    completeSection,
+    completeLesson,
+  } = require("./controller");
 
-router.get("/", (req, res) => {
-  res.send("routes home");
-});
+router.get("/", courseCompletion);
+router.get("/:courseId/", sectionCompletion);
+router.get("/:courseId/:sectionId/", lessonCompletion);
+
+router.put("/:courseId", completeCourse);
+router.put("/:courseId/:sectionId", completeSection);
+router.put("/:courseId/:sectionId/:lessonId", completeLesson);
+module.exports = router;
