235b33c12958057735c441f1481f4978bf0e8cc0
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-21 14:47:52 +0300

    Footer added

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Logo from '../assets/Logo/logo.png';
+import LogoFooter from '../assets/Logo/logo-footer.png';
 
 const Footer = () =>{
-    
+    return(
+            <div className="row bg-dark">
+                <div className="col-md-12">
+                    <div className="footer">
+                        <div className="row">
+                            <div className="col-md-2 col-12">
+                                <div className="footer-logo">
+                                    <img src={LogoFooter} alt="logo" />
+                                </div>
+                            </div>
+                            <div className="col-md-3 col-12 text-white mt-2">
+                                  <p>Utopix @2022 </p>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    );
 } 
 
 export default Footer;
