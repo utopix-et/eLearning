f104b602ee1554b1700d9e1a13b83eb887f5e366
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-25 19:51:02 +0300

    Course instructor image and name space added

--- frontend/src/pages/CourseDetail.js
+++ frontend/src/pages/CourseDetail.js
@@ -1,19 +1,35 @@
 import React from "react";
-import '../style/Course-detail.css'
+import "../style/Course-detail.css";
 
 import { GoPrimitiveDot } from "react-icons/go";
 import { WiMoonWaningCrescent1 } from "react-icons/wi";
-import {BsFillTriangleFill} from 'react-icons/bs';
-import {AiOutlineDownload} from 'react-icons/ai';
+import { BsFillTriangleFill } from "react-icons/bs";
+import { AiOutlineDownload } from "react-icons/ai";
+import Yidnek from "../assets/Images/Instructer/yidnek.jpg";
 
 const CourseDetail = () => {
   return (
     <div className="container bg-primary">
       <div className="row">
         <div className="col-md-6 bg-warning left-detail">
-          <p className="text-white course-title">Frontend Foundation HTML + CSS</p>
-          <GoPrimitiveDot className="text-success"/> 
+          <p className="text-white course-title">
+            Frontend Foundation HTML + CSS
+          </p>
+          <GoPrimitiveDot className="text-success" />
           <span className="text-white"> Beginner </span>
+
+          <div className="d-flex justify-content-between mt-3">
+            <div className="d-flex">
+              <img
+                src={Yidnek}
+                className="rounded-circle"
+                alt="profile-img"
+                width="30px"
+                height="30px"
+              />
+              <p className="ms-2">John Doe</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
