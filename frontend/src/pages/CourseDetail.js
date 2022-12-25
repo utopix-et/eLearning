f0268cf100a3457de83acd2ecf3a56723f2f0efe
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-25 20:07:25 +0300

    Course Detail first card final commit

--- frontend/src/pages/CourseDetail.js
+++ frontend/src/pages/CourseDetail.js
@@ -2,7 +2,9 @@ import React from "react";
 import "../style/Course-detail.css";
 
 import { GoPrimitiveDot } from "react-icons/go";
-import {IoMdTime} from "react-icons/io";
+import { IoMdTime } from "react-icons/io";
+import {BiDollarCircle} from 'react-icons/bi';
+import {AiOutlineDownload} from 'react-icons/ai';
 
 import Yidnek from "../assets/Images/Instructer/yidnek.jpg";
 
@@ -32,14 +34,33 @@ const CourseDetail = () => {
         </div>
 
         <div className="col-md-6 course-detail">
-           <h5 className="text-white"> 
-            About this Course
-            </h5>
-            <p className="text-white">
+          <h5 className="text-white">About this Course</h5>
+          <p className="text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-        </div>
+          </p>
+          
+          <div className="row mt-5">
+          <div className="col-md-5">
+              <IoMdTime className="text-white" size={35}/>
+              <span className="ms-2 text-white"> 3 Months </span>
+            </div>
 
+        <div className="col-md-5">
+            <BiDollarCircle className="text-white" size={35}/>
+            <span className="ms-2 text-white"> Free </span>
+        </div> 
+        </div>  
+
+        <div className="row mt-4 pt-3">
+              <div className="col-md-12">
+              <a className="text-dark text-decoration-none" href="download">
+                <AiOutlineDownload className="text-white me-1" />
+                <span className="text-white"> Download the course outline </span>
+              </a>
+            </div>
+            </div> 
+
+        </div>
       </div>
     </div>
   );
