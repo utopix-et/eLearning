e6005dc9c9497945b5ea4581d926644ba0891d23
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-25 20:12:01 +0300

    Course Detail Nav file created

---
+++ frontend/src/components/CourseDetail-nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../style/Course-detail.css'
+
+const CourseDetailNav = () => {
+    return (
+        <div>
+            <h1>jg</h1>
+        </div>
+    )
+}
+
+export default CourseDetailNav;
