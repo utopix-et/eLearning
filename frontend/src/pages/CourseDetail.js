093a4de9817d8820543b91275a10a0beca82cdc1
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-25 19:32:23 +0300

    CourseDetail css file created with Course-title

--- frontend/src/pages/CourseDetail.js
+++ frontend/src/pages/CourseDetail.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
+import '../assets/css/CourseDetail.css'
 
 const CourseDetail = () => {
-    return (
-        <div className='container'>
-            <div className='row'>
-                <div className='col-md-12'>
-                    test
-                                        </div>
-                                        </div>
-                                        </div>
-
-
-    )
-}
+  return (
+    <div className="container bg-primary">
+      <div className="row">
+        <div className="col-md-6 bg-warning">
+          <p className="my-4 ms-5 text-white course-title">Frontend Foundation HTML + CSS</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CourseDetail;
