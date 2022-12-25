3b2d5064503efeb14b3326eefd9df04b118f7a92
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-25 20:31:39 +0300

    Secondary navbar reponsivenss issue resoved

--- frontend/src/components/CourseDetail-nav.js
+++ frontend/src/components/CourseDetail-nav.js
@@ -6,11 +6,7 @@ const CourseDetailNav = () => {
         <div className='container'>
         <nav className="navbar navbar-expand-lg navbar-light bg-gray">
             <div className="container">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-3">
+                    <ul className="navbar-nav mx-auto">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Course Details</a>
                         </li>
@@ -24,7 +20,6 @@ const CourseDetailNav = () => {
                             <a className="nav-link" href="#">Recommendations</a>
                         </li>
                     </ul>
-                </div>
             </div>
         </nav>
         </div>
