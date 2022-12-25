e52d72e188595f3e403377029b8c6445048dbd97
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-12-25 19:48:20 +0300

    Difficulty of the course added to the coursedetail

--- frontend/src/pages/CourseDetail.js
+++ frontend/src/pages/CourseDetail.js
@@ -1,12 +1,19 @@
 import React from "react";
 import '../style/Course-detail.css'
 
+import { GoPrimitiveDot } from "react-icons/go";
+import { WiMoonWaningCrescent1 } from "react-icons/wi";
+import {BsFillTriangleFill} from 'react-icons/bs';
+import {AiOutlineDownload} from 'react-icons/ai';
+
 const CourseDetail = () => {
   return (
     <div className="container bg-primary">
       <div className="row">
-        <div className="col-md-6 bg-warning">
+        <div className="col-md-6 bg-warning left-detail">
           <p className="text-white course-title">Frontend Foundation HTML + CSS</p>
+          <GoPrimitiveDot className="text-success"/> 
+          <span className="text-white"> Beginner </span>
         </div>
       </div>
     </div>
