c818c4ab3cac19e9ce7a49adaa1ab10f82b2b304
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-01 20:48:48 +0300

    Profile info ui changed for the code integration

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -3,31 +3,30 @@ import React from "react";
 import "../style/Profile.css";
 
 import Cover from "../assets/Images/Profile/cover.jpg";
+import Avatar from "../assets/Images/Profile/yidnek.jpg";
 
 const Profile = () => {
   return (
     <div className="container-fluid bg-light">
-      <div className="row">
-     
-        <div
-          className="blue-gradient text-center text-white p-5 border rounded"
-          style={{ fontSize: "1.6rem" }}
-        >
-           <div className="col-md-12 col-12 pb-3 ms-3">
+      <div
+        className="row blue-gradient text-center text-white p-5 border rounded"
+        style={{ fontSize: "1.6rem" }}
+      >
+        <div className="col-md-12 col-12">
           UTOPIX <span style={{ fontSize: "1rem" }}>E-LEARNING</span> COHORT#1
-          </div>
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-            className="rounded-circle z-depth-0 mt-2 avatar-img"
-            style={{width: '5.5rem'}}
-            alt="Avatar"
-          />
         </div>
       </div>
-
       <div className="row mt-3">
-        <div className="col-md-12 col-12 text-center text-dark mt-3">
+        <div className="col-md-5 col-12 mx-auto text-center text-dark">
           PROFILE INFORMATION{" "}
+        </div>
+        <div className="col-md-12 col-12 mt-3">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            className="rounded-circle text-center mx-auto d-block"
+            style={{ width: "5.5rem" }}
+            alt="Avatar"
+          />
         </div>
       </div>
     </div>
