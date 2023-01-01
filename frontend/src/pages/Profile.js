ee6575936d8bd8b6f78bc641469c3825c2c0ce15
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-01 19:48:54 +0300

    Minor syntax editing

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import '../style/Profile.css';
+import "../style/Profile.css";
 
 import Cover from "../assets/Images/Profile/cover.jpg";
 
@@ -9,15 +9,16 @@ const Profile = () => {
     <div className="container-fluid bg-light">
       <div className="row">
         <div className="col-md-12 col-12">
-          <div className="cover">
-        <div className="bg-primary text-center text-white p-5 border rounded" style={{fontSize: '1.6rem'}}>
-           UTOPIX <span style={{fontSize: '1rem'}}>E-LEARNING</span> COHORT#1
-      </div>
-    
-          </div>  
+            <div
+              className="bg-primary text-center text-white p-5 border rounded"
+              style={{ fontSize: "1.6rem" }}
+            >
+              UTOPIX <span style={{ fontSize: "1rem" }}>E-LEARNING</span>{" "}
+              COHORT#1
+          </div>
         </div>
       </div>
-      </div>
+    </div>
   );
 };
 
