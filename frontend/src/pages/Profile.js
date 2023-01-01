97042ff660060c0e3ea2599b9bd3aa10e33ef028
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-01 20:50:56 +0300

    minor editing funcitonality for photo added

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -17,16 +17,17 @@ const Profile = () => {
         </div>
       </div>
       <div className="row mt-3">
-        <div className="col-md-5 col-12 mx-auto text-center text-dark">
+        <div className="col-md-12 col-12 mx-auto text-center text-dark">
           PROFILE INFORMATION{" "}
         </div>
-        <div className="col-md-12 col-12 mt-3">
+        <div className="col-md-12 col-12 mt-3 text-center">
           <img
             src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-            className="rounded-circle text-center mx-auto d-block"
+            className="rounded-circle text-center mx-auto d-block pb-1"
             style={{ width: "5.5rem" }}
             alt="Avatar"
           />
+          <span className="text-decoration-underline">Edit</span>
         </div>
       </div>
     </div>
