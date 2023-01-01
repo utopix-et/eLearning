0a9dd70e34cb8f949492ffb479c9416f228f159c
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-01 20:20:56 +0300

    profile avatar created with and additional class

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -8,14 +8,26 @@ const Profile = () => {
   return (
     <div className="container-fluid bg-light">
       <div className="row">
-        <div className="col-md-12 col-12">
-            <div
-              className="blue-gradient color-block z-depth-1 text-center text-white p-5 border rounded"
-              style={{ fontSize: "1.6rem" }}
-            >
-              UTOPIX <span style={{ fontSize: "1rem" }}>E-LEARNING</span>{" "}
-              COHORT#1
+     
+        <div
+          className="blue-gradient text-center text-white p-5 border rounded"
+          style={{ fontSize: "1.6rem" }}
+        >
+           <div className="col-md-12 col-12 pb-3 ms-3">
+          UTOPIX <span style={{ fontSize: "1rem" }}>E-LEARNING</span> COHORT#1
           </div>
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            className="rounded-circle z-depth-0 mt-2 avatar-img"
+            style={{width: '5.5rem'}}
+            alt="Avatar"
+          />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-12 col-12 text-center text-dark mt-3">
+          PROFILE INFORMATION{" "}
         </div>
       </div>
     </div>
